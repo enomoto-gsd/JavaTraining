e9bd7ae2fbd421322bba67c6cdcf6917c9e6b4e0
enomoto <enomoto@gsd-ltd.co.jp>
2022-03-04 15:44:10 +0900

    Java 入門初級　第1回の課題です。

---
+++ work/Work1.java
@@ -0,0 +1,7 @@
+package work;
+
+public class Work1 {
+	public static void main(String[] args) {
+		System.out.println("„‚Ì–¼‘O‚Í‰|–{ˆê÷‚Å‚·B");
+	}
+}
---
+++ work/Work2.java
@@ -0,0 +1,11 @@
+package work;
+
+public class Work2 {
+	public static void main(String[] args) {
+		System.out.println("私の名前は榎本一樹です。");
+		System.out.println("今はJavaを学習しております。");
+
+		//以下、個人的な記述
+
+	}
+}
