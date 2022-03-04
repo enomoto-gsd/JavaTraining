657f2c9b9095cf1074baa0e55bd0d0d844dff89f
enomoto <enomoto@gsd-ltd.co.jp>
2022-03-04 16:06:58 +0900

    Java 入門初級　第2回の課題の続きです。

---
+++ work2CreateProject/Work1.java
@@ -0,0 +1,11 @@
+package work2CreateProject;
+
+public class Work1 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		System.out.println("x‚Æ‚™‚Ì‡Œv‚Í" +(x+y));
+
+	}
+
+}
